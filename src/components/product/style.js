b4ff42588de816d4cae9bb5c672b3049c15654c1
vladbashkov <vlad@cgspark.com>
2024-04-26 18:14:54 +0300

    Shadow added

--- src/components/product/style.js
+++ src/components/product/style.js
@@ -6,8 +6,16 @@ const styles = StyleSheet.create({
 		padding: 10,
 		borderWidth: 1,
 		borderColor: "#000",
+		backgroundColor: "#fff",
 		borderRadius: 10,
 		marginBottom: 10,
+		// Тінь для iOS
+		shadowColor: "#000",
+		shadowOffset: { width: 0, height: 3 },
+		shadowOpacity: 0.6,
+		shadowRadius: 2,
+		// Тінь для Android
+		elevation: 5,
 	},
 	details: {
 		flex: 2,
