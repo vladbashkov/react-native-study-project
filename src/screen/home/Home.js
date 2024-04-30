87ee2f9fb7c6aeab4352ae019ffd272acdb62f67
vladbashkov <vlad@cgspark.com>
2024-04-30 21:55:53 +0300

    New items for app banner added

--- src/screen/home/Home.js
+++ src/screen/home/Home.js
@@ -1,14 +1,20 @@
-import { View } from "react-native";
+import { View, FlatList } from "react-native";
+import { useState } from "react";
 
 import Product from "../../components/product/product";
+import AppBar from "../appBar/AppBar";
+
 import products from "../../mock/products";
 
 const Home = () => {
+	const [searchText, setSearchText] = useState("");
+
+	const filteredProducts = products.filter((product) => product.title.toLowerCase().includes(searchText.toLowerCase()));
+
 	return (
 		<View>
-			{products.map((product, index) => {
-				return <Product key={index} product={product} />;
-			})}
+			<AppBar onSearch={setSearchText} />
+			<FlatList data={filteredProducts} keyExtractor={products.title} renderItem={({ item }) => <Product key={item.title} product={item} />} />
 		</View>
 	);
 };
