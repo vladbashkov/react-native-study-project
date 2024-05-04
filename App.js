b81c266821cf89afad745acc9944b1525825e9fc
vladbashkov <vlad@cgspark.com>
2024-05-04 18:38:49 +0300

    App.js minor code improvement

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, View } from "react-native";
+import { SafeAreaView } from "react-native";
 
 import Home from "./src/screen/home/Home";
 
@@ -7,9 +7,7 @@ export default function App() {
 	return (
 		<SafeAreaView>
 			<StatusBar barStyle="dark-content" backgroundColor="#ffffff" translucent={false} />
-			<View style={{ borderTopWidth: 1, borderTopColor: "#ffb568", paddingTop: 10, paddingBottom: 40, paddingHorizontal: 10 }}>
-				<Home />
-			</View>
+			<Home />
 		</SafeAreaView>
 	);
 }
