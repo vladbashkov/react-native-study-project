248c46c4d2409a4852852001f05c23f88433982e
vladbashkov <vlad@cgspark.com>
2024-05-08 08:58:58 +0300

    Color theme appearance in progress

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, useColorScheme } from "react-native";
 
 import Home from "./src/screen/home/Home";
 
 export default function App() {
+	const colorScheme = useColorScheme();
+
 	return (
-		<SafeAreaView style={{ flex: 1 }}>
+		<SafeAreaView style={{ flex: 1, backgroundColor: colorScheme === "dark" ? "#666" : "#fff" }}>
 			<StatusBar barStyle="dark-content" backgroundColor="#ffffff" translucent={false} />
 			<Home />
 		</SafeAreaView>
