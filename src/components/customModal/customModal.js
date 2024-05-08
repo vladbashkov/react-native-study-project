5e2dcb0edc7898aeeb448e2223bdcb9833d657e3
vladbashkov <vlad@cgspark.com>
2024-05-08 08:59:17 +0300

    Custom modal minor improvements

--- src/components/customModal/customModal.js
+++ src/components/customModal/customModal.js
@@ -13,13 +13,11 @@ export const CustomModal = ({ isVisible, onClose, children, styleVariant }) => {
 		<Modal animationType="fade" transparent={true} visible={isVisible} onRequestClose={onClose}>
 			<TouchableWithoutFeedback onPress={handleOutsideClick}>
 				<View style={styles.modalOverlay}>
-					<TouchableWithoutFeedback>
-						<View style={styles.modalContent}>
-							<Pressable title="Close" onPress={onClose}>
-								{children}
-							</Pressable>
-						</View>
-					</TouchableWithoutFeedback>
+					<View style={styles.modalContent}>
+						<Pressable title="Close" onPress={onClose}>
+							{children}
+						</Pressable>
+					</View>
 				</View>
 			</TouchableWithoutFeedback>
 		</Modal>
